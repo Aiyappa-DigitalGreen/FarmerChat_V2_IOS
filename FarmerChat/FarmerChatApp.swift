10885f55c0eb03b8048c6e26940db004649d7b89
Aiyappa <126744250+Aiyappa-DigitalGreen@users.noreply.github.com>
2026-02-11 03:45:08 +0530

    Initial Commit

---
+++ FarmerChat/FarmerChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmerChatApp.swift
+//  FarmerChat
+//
+//  Created by Aiyappa  Mahalingam on 11/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FarmerChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
